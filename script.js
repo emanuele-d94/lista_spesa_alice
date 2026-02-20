5883721a516b5df003fa288dceb6ec9bda20c3d0
emanuele-midori <emanuele@midorisrl.eu>
2026-02-20 18:30:45 +0100

    Articoli acquistati barrati
    Pulsante rimuovi

--- script.js
+++ script.js
@@ -7,9 +7,49 @@ const bottone = document.querySelector('.button');
 
 bottone.addEventListener('click', (event) => {
     event.preventDefault();
+
     if (input.value.trim() === '') return; // evita elementi vuoti
+
     let articolo = document.createElement("li");
-    articolo.textContent = input.value;
+
+    let articoloDiv = document.createElement('div')
+    articoloDiv.classList.add('articoloDiv');
+
+    let articoloDivText = document.createElement("p");
+    articoloDivText.classList.add('articoloDivText');
+    articoloDivText.textContent = input.value;
+
+    articoloDivText.addEventListener('click', (event) => {
+        event.preventDefault();
+        // Articolo acquistato viene barrato
+        if(articoloDivText.style.color === 'grey'){
+            articoloDivText.style.textDecoration = "none";
+            articoloDivText.style.color = "#456967"
+        } else {
+            articoloDivText.style.textDecoration = "line-through";
+            articoloDivText.style.color = "grey";
+        }
+    })
+
+    articoloDiv.appendChild(articoloDivText);
+
+
+    let articoloDivButton = document.createElement("button");
+    articoloDivButton.classList.add('articoloDivButton');
+    articoloDivButton.textContent = 'Rimuovi'
+
+    articoloDivButton.addEventListener('click', (event) => {
+        //Rimuovi articolo
+        event.preventDefault();
+        articolo.remove()
+    })
+
+    articoloDiv.appendChild(articoloDivButton);
+
+
+    articolo.appendChild(articoloDiv);
+
     lista.appendChild(articolo);
     input.value = '';
 })
+
